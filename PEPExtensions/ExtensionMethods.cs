d1531bc6cfe34c62da1956af4ccbab7dc50d41b0
Inwerwm <saclainvervm@gmail.com>
2020-07-12 11:41:44 +0900

    作成

---
+++ PEPExtensions/ExtensionMethods.cs
@@ -0,0 +1,179 @@
+﻿using PEPlugin.Pmx;
+using PEPlugin.SDX;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEPExtensions
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// 指定された閉区間の範囲内であるかを判断します。
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="lower">下限（自身も含む）</param>
+        /// <param name="upper">上限（自身も含む）</param>
+        /// <returns></returns>
+        public static bool IsWithin<T>(this T i, T lower, T upper) where T : IComparable
+        {
+            if (upper.CompareTo(lower) < 0)
+                throw new ArgumentOutOfRangeException("IsWithin<T>:下限値が上限値よりも大きいです。");
+            return i.CompareTo(lower) * upper.CompareTo(i) >= 0;
+        }
+
+        /// <summary>
+        /// 指定された開区間の範囲内であるかを判断します。
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="lower">下限（自身は含まない）</param>
+        /// <param name="upper">上限（自身は含まない）</param>
+        /// <returns></returns>
+        public static bool IsInside<T>(this T i, T lower, T upper) where T : IComparable
+        {
+            if (upper.CompareTo(lower) < 0)
+                throw new ArgumentOutOfRangeException("IsInside<T>:下限値が上限値よりも大きいです。");
+            return i.CompareTo(lower) * upper.CompareTo(i) > 0;
+        }
+
+        public enum Interval
+        {
+            Open,
+            Close
+        }
+
+        /// <summary>
+        /// 指定した区間の範囲内であるかを判断します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="i"></param>
+        /// <param name="lower">下限</param>
+        /// <param name="lowerInterval">左側の開閉</param>
+        /// <param name="upper">上限</param>
+        /// <param name="upperInterval">右側の開閉</param>
+        /// <returns></returns>
+        public static bool IsInRangeOf<T>(this T i, T lower, Interval lowerInterval, T upper, Interval upperInterval) where T : IComparable
+        {
+            if (upper.CompareTo(lower) < 0)
+                throw new ArgumentOutOfRangeException("IsInRangeOf<T>:下限値が上限値よりも大きいです。");
+
+            var l = i.CompareTo(lower);
+            bool isInRangeLower = lowerInterval == Interval.Close ? l >= 0 : l > 0;
+            var u = upper.CompareTo(i);
+            bool isInRangeUpper = upperInterval == Interval.Close ? u >= 0 : u > 0;
+
+            return isInRangeLower && isInRangeUpper;
+        }
+
+        /// <summary>
+        /// stringに変換して末尾に改行文字を加える
+        /// </summary>
+        public static string ToStringLine(this object value)
+        {
+            return value.ToString() + Environment.NewLine;
+        }
+
+        /// <summary>
+        /// 「名前 = 値」の形で出力する
+        /// </summary>
+        /// <param name="name">名前</param>
+        public static string ToStringName(this object value, string name)
+        {
+            return $"{name} = {value}";
+        }
+
+        /// <summary>
+        /// 「名前 = 値↲」の形で出力する
+        /// </summary>
+        /// <param name="name">名前</param>
+        public static string ToStringNL(this object value, string name)
+        {
+            return $"{name} = {value.ToStringLine()}";
+        }
+
+        public static float[] ToArray(this V3 v)
+        {
+            return new float[] { v.X, v.Y, v.Z };
+        }
+
+        public static float[] ToArray(this V2 v)
+        {
+            return new float[] { v.X, v.Y };
+        }
+
+        public static PointF ToPointF(this V2 vertex)
+        {
+            return new PointF(vertex.X, vertex.Y);
+        }
+
+        public static PointF ToPointF(this V2 vertex, int Width, int Height)
+        {
+            return new PointF(vertex.X * Width, vertex.Y * Height);
+        }
+
+        public static PointF[] ToPointF(this IPXFace face)
+        {
+            return new PointF[3] { face.Vertex1.UV.ToPointF(), face.Vertex2.UV.ToPointF(), face.Vertex3.UV.ToPointF() };
+        }
+
+        public static PointF[] ToPointF(this IPXFace face, int Width, int Height)
+        {
+            return new PointF[3] { face.Vertex1.UV.ToPointF(Width, Height), face.Vertex2.UV.ToPointF(Width, Height), face.Vertex3.UV.ToPointF(Width, Height) };
+        }
+
+        public static Point ToPoint(this V2 vertex)
+        {
+            return new Point((int)Math.Round(vertex.X, MidpointRounding.AwayFromZero), (int)Math.Round(vertex.Y, MidpointRounding.AwayFromZero));
+        }
+
+        public static Point ToPoint(this V2 vertex, int Width, int Height)
+        {
+            return new Point((int)Math.Round(vertex.X * Width, MidpointRounding.AwayFromZero), (int)Math.Round(vertex.Y * Height, MidpointRounding.AwayFromZero));
+        }
+
+        public static Point[] ToPoint(this IPXFace face)
+        {
+            return new Point[3] { face.Vertex1.UV.ToPoint(), face.Vertex2.UV.ToPoint(), face.Vertex3.UV.ToPoint() };
+        }
+
+        public static Point[] ToPoint(this IPXFace face, int Width, int Height)
+        {
+            return new Point[3] { face.Vertex1.UV.ToPoint(Width, Height), face.Vertex2.UV.ToPoint(Width, Height), face.Vertex3.UV.ToPoint(Width, Height) };
+        }
+
+        /// <summary>
+        /// 四捨五入で整数化
+        /// </summary>
+        public static int Round(this float value) => (int)Math.Round(value, MidpointRounding.AwayFromZero);
+
+        /// <summary>
+        /// 四捨五入で整数化
+        /// </summary>
+        public static int Round(this double value) => (int)Math.Round(value, MidpointRounding.AwayFromZero);
+
+        /// <summary>
+        /// 四捨五入で整数化
+        /// </summary>
+        public static int Round(this decimal value) => (int)Math.Round(value, MidpointRounding.AwayFromZero);
+
+        public static string Print(this Point point) => $"({point.X}, {point.Y})";
+        public static string Print(this PointF point) => $"({point.X}, {point.Y})";
+
+        public static V2[] ExtructUV(this IPXFace face)
+        {
+            return new V2[]
+            {
+                face.Vertex1.UV,
+                face.Vertex2.UV,
+                face.Vertex3.UV
+            };
+        }
+
+        public static string Print(this V2 v) => $"({v.X}, {v.Y})";
+        public static string Print(this V3 v) => $"({v.X}, {v.Y}), {v.Z}";
+        public static string PrintUV(this IPXFace f) => $"{f.Vertex1.UV.Print()}, {f.Vertex2.UV.Print()}, {f.Vertex3.UV.Print()}";
+    }
+}
---
+++ PEPExtensions/Utility.cs
@@ -0,0 +1,207 @@
+﻿using PEPlugin;
+using PEPlugin.Pmx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PEPExtensions
+{
+    public static class Utility
+    {
+        /// <summary>
+        /// モデル・フォーム・ビューを一括更新する
+        /// </summary>
+        /// <param name="connector">ホストへのコネクタ</param>
+        /// <param name="pmx">更新用プラグインPMX</param>
+        /// <param name="option">更新対象</param>
+        /// <param name="index">任意の対象Index</param>
+        public static void Update(IPEConnector connector, IPXPmx pmx, PmxUpdateObject option = PmxUpdateObject.All, int index = -1)
+        {
+            connector.Pmx.Update(pmx, option, index);
+            connector.Form.UpdateList(ConvUObjrct_DtoX(option));
+            connector.View.PmxView.UpdateModel();
+        }
+
+        /// <summary>
+        /// PmxUpdateObjectからPmd.UpdateObjectに変換する
+        /// </summary>
+        /// <param name="input">変換するPmxUpdateObject</param>
+        /// <returns></returns>
+        public static PEPlugin.Pmd.UpdateObject ConvUObjrct_DtoX(PmxUpdateObject input)
+        {
+            PEPlugin.Pmd.UpdateObject output;
+            switch (input)
+            {
+                case PmxUpdateObject.None:
+                    output = PEPlugin.Pmd.UpdateObject.None;
+                    break;
+                case PmxUpdateObject.All:
+                    output = PEPlugin.Pmd.UpdateObject.All;
+                    break;
+                case PmxUpdateObject.Header:
+                    output = PEPlugin.Pmd.UpdateObject.Header;
+                    break;
+                case PmxUpdateObject.ModelInfo:
+                    output = PEPlugin.Pmd.UpdateObject.All;
+                    break;
+                case PmxUpdateObject.Vertex:
+                    output = PEPlugin.Pmd.UpdateObject.Vertex;
+                    break;
+                case PmxUpdateObject.Face:
+                    output = PEPlugin.Pmd.UpdateObject.Face;
+                    break;
+                case PmxUpdateObject.Material:
+                    output = PEPlugin.Pmd.UpdateObject.Material;
+                    break;
+                case PmxUpdateObject.Bone:
+                    output = PEPlugin.Pmd.UpdateObject.Bone;
+                    break;
+                case PmxUpdateObject.Morph:
+                    output = PEPlugin.Pmd.UpdateObject.Morph;
+                    break;
+                case PmxUpdateObject.Node:
+                    output = PEPlugin.Pmd.UpdateObject.Node;
+                    break;
+                case PmxUpdateObject.Body:
+                    output = PEPlugin.Pmd.UpdateObject.Body;
+                    break;
+                case PmxUpdateObject.Joint:
+                    output = PEPlugin.Pmd.UpdateObject.Joint;
+                    break;
+                case PmxUpdateObject.SoftBody:
+                    output = PEPlugin.Pmd.UpdateObject.All;
+                    break;
+                default:
+                    output = PEPlugin.Pmd.UpdateObject.All;
+                    break;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// 入力頂点のボーン・ウェイトをタプルリストに変換する
+        /// </summary>
+        /// <param name="vertex">入力頂点</param>
+        /// <returns>(ボーン,ウェイト)のタプルリスト</returns>
+        public static List<(IPXBone bone, float weight)> GetWeights(IPXVertex vertex)
+        {
+            var weights = new List<(IPXBone bone, float weight)>();
+            weights.Add((vertex.Bone1, vertex.Weight1));
+            weights.Add((vertex.Bone2, vertex.Weight2));
+            weights.Add((vertex.Bone3, vertex.Weight3));
+            weights.Add((vertex.Bone4, vertex.Weight4));
+            weights.RemoveAll(w => w.bone == null);
+            weights.RemoveAll(w => w.weight == 0.0);
+            return weights;
+        }
+
+        /// <summary>
+        /// タプルリストから頂点にボーン・ウェイト情報を正規化して格納する
+        /// </summary>
+        /// <param name="weights">(ボーン,ウェイト)タプルのリスト</param>
+        /// <param name="vertex">格納する頂点</param>
+        public static void SetVertexWeights(List<(IPXBone bone, float weight)> weights, ref IPXVertex vertex)
+        {
+            //正規化
+            var weight = NormalizeWeights(weights);
+            ClearVertexWeight(ref vertex);
+
+            //頂点に入力
+            vertex.Bone1 = weight[0].bone;
+            vertex.Weight1 = weight[0].weight;
+            if (weight.Count > 1)
+            {
+                vertex.Bone2 = weight[1].bone;
+                vertex.Weight2 = weight[1].weight;
+            }
+            if (weight.Count > 2)
+            {
+                vertex.Bone3 = weight[2].bone;
+                vertex.Weight3 = weight[2].weight;
+            }
+            if (weight.Count > 3)
+            {
+                vertex.Bone4 = weight[3].bone;
+                vertex.Weight4 = weight[3].weight;
+            }
+        }
+
+        /// <summary>
+        /// ウェイトを正規化する
+        /// </summary>
+        /// <param name="weights">正規化する(ボーン,ウェイト)タプルのリスト</param>
+        /// <param name="basis">正規化後の合計</param>
+        /// <returns>正規化された(ボーン,ウェイト)タプルのリスト</returns>
+        public static List<(IPXBone bone, float weight)> NormalizeWeights(List<(IPXBone bone, float weight)> weights, float basis = 1)
+        {
+            //weightを基準に降順でソート
+            weights.Sort((v, w) => w.weight.CompareTo(v.weight));
+
+            //大きい方から4つまでを残し正規化
+            var sum = weights.Take(4).Select(w => w.weight).Sum();
+            return weights.Take(4).Select(w => (w.bone, basis * w.weight / sum)).ToList();
+        }
+
+        /// <summary>
+        /// ウェイトを正規化する
+        /// </summary>
+        /// <param name="vertex">正規化する頂点</param>
+        public static void NormalizeWeights(ref IPXVertex vertex)
+        {
+            SetVertexWeights(GetWeights(vertex), ref vertex);
+        }
+
+        public static void ClearVertexWeight(ref IPXVertex vertex)
+        {
+            vertex.Bone1 = null;
+            vertex.Weight1 = 0;
+            vertex.Bone2 = null;
+            vertex.Weight2 = 0;
+            vertex.Bone3 = null;
+            vertex.Weight3 = 0;
+            vertex.Bone4 = null;
+            vertex.Weight4 = 0;
+        }
+
+        public static List<IPXVertex> GetFaceVertices(IPXFace face)
+        {
+            var vertices = new List<IPXVertex>();
+            vertices.Add(face.Vertex1);
+            vertices.Add(face.Vertex2);
+            vertices.Add(face.Vertex3);
+            return vertices;
+        }
+
+        public static List<IPXVertex> GetMaterialVertices(IPXMaterial material)
+        {
+            var vertices = new List<IPXVertex>();
+            foreach (var f in material.Faces)
+            {
+                vertices.AddRange(GetFaceVertices(f));
+            }
+            return vertices.Distinct().ToList();
+        }
+
+        public static DialogResult ShowException(Exception ex)
+        {
+            // Get reference to the dialog type. 
+            var dialogTypeName = "System.Windows.Forms.PropertyGridInternal.GridErrorDlg";
+            var dialogType = typeof(Form).Assembly.GetType(dialogTypeName);
+
+            // Create dialog instance. 
+            var dialog = (Form)Activator.CreateInstance(dialogType, new PropertyGrid());
+
+            // Populate relevant properties on the dialog instance. 
+            dialog.Text = "例外発生";
+            dialogType.GetProperty("Details").SetValue(dialog, ex.StackTrace, null);
+            dialogType.GetProperty("Message").SetValue(dialog, ex.Message, null);
+
+            // Display dialog. 
+            return dialog.ShowDialog();
+        }
+    }
+}
