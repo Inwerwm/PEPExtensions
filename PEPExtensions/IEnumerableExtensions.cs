c91e7423276adeccfdad0d92705201112ff26f54
Inwerwm <saclainvervm@gmail.com>
2020-12-05 21:00:36 +0900

    面と頂点関連メソッドを追加

---
+++ PEPExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEPExtensions
+{
+    /// <summary>
+    /// IEnumerableの拡張メソッド
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// 対象のコレクションの全要素を自身も格納しているかを調べる
+        /// </summary>
+        /// <typeparam name="T">要素の型</typeparam>
+        /// <param name="basis">比較元コレクション</param>
+        /// <param name="collection">比較先コレクション</param>
+        /// <returns>すべての要素を含んでいるか</returns>
+        public static bool Contains<T>(this IEnumerable<T> basis, IEnumerable<T> collection) => collection.Aggregate(true, (sum, elm) => sum && basis.Contains(elm));
+    }
+}
---
+++ PEPExtensions/IPXExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PEPlugin.Pmx;
+
+namespace PEPExtensions
+{
+    /// <summary>
+    /// IPX系の拡張メソッド
+    /// </summary>
+    public static class IPXExtensions
+    {
+        /// <summary>
+        /// 頂点配列に変換
+        /// </summary>
+        /// <param name="face">変換元面</param>
+        /// <returns>面を構成する頂点の配列</returns>
+        public static IPXVertex[] ToVertices(this IPXFace face) => new IPXVertex[] { face.Vertex1, face.Vertex2, face.Vertex3 };
+        /// <summary>
+        /// 頂点インデックス配列に変換
+        /// </summary>
+        /// <param name="face">変換元面</param>
+        /// <param name="pmx">頂点を内包したPMXデータ</param>
+        /// <returns>面を構成する頂点のインデックス配列</returns>
+        public static int[] ToVertexIndices(this IPXFace face, IPXPmx pmx) => new int[] { pmx.Vertex.IndexOf(face.Vertex1), pmx.Vertex.IndexOf(face.Vertex2), pmx.Vertex.IndexOf(face.Vertex3) };
+    }
+}
