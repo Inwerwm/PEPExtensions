5f18638a77299256abb7e74f36dd137f5f5a7efd
Inwerwm <saclainvervm@gmail.com>
2021-02-06 19:58:21 +0900

    面の頂点インデックス化拡張メソッドの高速化

--- PEPExtensions/IPXExtensions.cs
+++ PEPExtensions/IPXExtensions.cs
@@ -19,11 +19,31 @@ namespace PEPExtensions
         /// <returns>面を構成する頂点の配列</returns>
         public static IPXVertex[] ToVertices(this IPXFace face) => new IPXVertex[] { face.Vertex1, face.Vertex2, face.Vertex3 };
         /// <summary>
-        /// 頂点インデックス配列に変換
+        /// <para>頂点インデックス配列に変換</para>
+        /// <para>使用数が多いようなら材質のFaceToVertexIndices拡張メソッドの使用を検討すること</para>
+        /// <para>頂点インデックス辞書の作成分のオーバーヘッドが発生するため</para>
         /// </summary>
         /// <param name="face">変換元面</param>
         /// <param name="pmx">頂点を内包したPMXデータ</param>
         /// <returns>面を構成する頂点のインデックス配列</returns>
-        public static int[] ToVertexIndices(this IPXFace face, IPXPmx pmx) => new int[] { pmx.Vertex.IndexOf(face.Vertex1), pmx.Vertex.IndexOf(face.Vertex2), pmx.Vertex.IndexOf(face.Vertex3) };
+        public static int[] ToVertexIndices(this IPXFace face, IPXPmx pmx)
+        {
+            var vtxIds = pmx.Vertex.Select((v, i) => (v, i)).ToDictionary(p => p.v, p => p.i);
+            return new int[] { vtxIds[face.Vertex1], vtxIds[face.Vertex2], vtxIds[face.Vertex3] };
+        }
+        /// <summary>
+        /// 材質内のすべての面を頂点インデックス配列に変換
+        /// </summary>
+        /// <param name="material">変換する諸面の材質</param>
+        /// <param name="pmx">頂点を内包したPMXデータ</param>
+        /// <returns>材質内の全ての面を構成する頂点のインデックス配列</returns>
+        public static IEnumerable<int[]> FaceToVertexIndices(this IPXMaterial material, IPXPmx pmx)
+        {
+            var vtxIds = pmx.Vertex.Select((v, i) => (v, i)).ToDictionary(p => p.v, p => p.i);
+            foreach (var face in material.Faces)
+            {
+                yield return new int[] { vtxIds[face.Vertex1], vtxIds[face.Vertex2], vtxIds[face.Vertex3] };
+            }
+        }
     }
 }
