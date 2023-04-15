c62bdafaf74e498fab385c6ee141919fa2b41353
Inwerwm <saclainvervm@gmail.com>
2023-04-15 23:49:42 +0900

    IListにAddRangeの拡張メソッドを追加

--- PEPExtensions/IEnumerableExtensions.cs
+++ PEPExtensions/IEnumerableExtensions.cs
@@ -19,5 +19,19 @@ namespace PEPExtensions
         /// <param name="collection">比較先コレクション</param>
         /// <returns>すべての要素を含んでいるか</returns>
         public static bool Contains<T>(this IEnumerable<T> basis, IEnumerable<T> collection) => collection.Aggregate(true, (sum, elm) => sum && basis.Contains(elm));
+
+        /// <summary>
+        /// 指定したコレクションの要素を全て加える
+        /// </summary>
+        /// <typeparam name="T">要素の型</typeparam>
+        /// <param name="basis">元リスト</param>
+        /// <param name="collection">追加コレクション</param>
+        public static void AddRange<T>(this IList<T> basis, IEnumerable<T> collection)
+        {
+            foreach (var elm in collection)
+            {
+                basis.Add(elm);
+            }
+        }
     }
 }
